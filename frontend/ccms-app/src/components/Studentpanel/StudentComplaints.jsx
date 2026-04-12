317eda5176bf711c2a6291f50c4f4befd206ab6f
Sasijoy <124587665+sk191199@users.noreply.github.com>
2026-04-12 16:36:27 +0530

    enhance student complaints panel with status chips and detail dialog

--- frontend/ccms-app/src/components/Studentpanel/StudentComplaints.jsx
+++ frontend/ccms-app/src/components/Studentpanel/StudentComplaints.jsx
@@ -10,15 +10,23 @@ import {
   TableCell,
   TablePagination,
   Paper,
-  IconButton,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  Chip,
 } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import DeleteIcon from "@mui/icons-material/Delete";
+// import DeleteIcon from "@mui/icons-material/Delete";
 
 import { getAllComplaintsByUser } from "../../api/auth.api";
 
 const StudentComplaints = () => {
   const [myComplaints, setMyComplaints] = useState([]);
+  const [selectedComplaint, setSelectedComplaint] = useState(null);
+  const [statusMap, setStatusMap] = useState([]);
+  const [open, setOpen] = useState(false);
 
   // fetch complaints
   const fetchMyComplaints = async () => {
@@ -27,6 +35,14 @@ const StudentComplaints = () => {
       const data = response.data.data;
       console.log("res", data);
       setMyComplaints(data);
+
+      const map = {};
+
+      data.forEach((item) => {
+        map[item.complaint_id] = item.status;
+      });
+
+      setStatusMap(map);
     } catch (error) {
       console.log(error);
     }
@@ -36,6 +52,15 @@ const StudentComplaints = () => {
     fetchMyComplaints();
   }, []);
 
+  const getStatusColor = (status) => {
+    if (status === "pending") return "warning";
+    if (status === "in-progress") return "info";
+    if (status === "resolved") return "success";
+    if (status === "rejected") return "error";
+
+    return "default";
+  };
+
   return (
     <Box sx={{ p: 1 }}>
       <Typography variant="h6" color="initial">
@@ -44,11 +69,11 @@ const StudentComplaints = () => {
       {/* ===== table ===== */}
       <TableContainer component={Paper} sx={{ mt: 2 }}>
         <Table>
-          <TableHead sx={{backgroundColor:"#f1f1f1"}}>
+          <TableHead sx={{ backgroundColor: "#f1f1f1" }}>
             <TableRow>
               <TableCell>ID</TableCell>
               <TableCell>Title</TableCell>
-              <TableCell>Description</TableCell>
+
               <TableCell>Status</TableCell>
               <TableCell>Department</TableCell>
               <TableCell>Date</TableCell>
@@ -58,29 +83,89 @@ const StudentComplaints = () => {
           <TableBody>
             {myComplaints.map((item) => (
               <TableRow key={item.complaint_id}>
-                <TableCell>{item.complaint_id}</TableCell>
+                <TableCell>
+                  <Chip label={`# ${item.complaint_id}`} size="small" />
+                </TableCell>
                 <TableCell>{item.title}</TableCell>
-                <TableCell>{item.description}</TableCell>
-                <TableCell>{item.status}</TableCell>
+
+                <TableCell>
+                  <Chip
+                    label={statusMap[item.complaint_id]}
+                    size="small"
+                    color={getStatusColor(statusMap[item.complaint_id])}
+                  />
+                </TableCell>
                 <TableCell>{item.department.department_name}</TableCell>
                 <TableCell>
                   {new Date(item.created_at).toLocaleDateString()}
                 </TableCell>
                 <TableCell>
-                  <IconButton color="primary">
-                    <VisibilityIcon />
-                  </IconButton>
-                  <IconButton color="error">
-                    <DeleteIcon />
-                  </IconButton>
+                  <Button
+                    startIcon={<VisibilityIcon />}
+                    variant="outlined"
+                    onClick={() => {
+                      setSelectedComplaint(item);
+                      setOpen(true);
+                    }}
+                  >
+                    view
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
-          {/* <TablePagination component="div"></TablePagination> */}
-          <TablePagination ></TablePagination>
         </Table>
       </TableContainer>
+
+      {/* dialog  */}
+      <Dialog
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        maxWidth="sm"
+        fullWidth
+      >
+        {selectedComplaint && (
+          <>
+            <DialogTitle>Complaint Details</DialogTitle>
+            <DialogContent>
+              <Typography variant="subtitle1" color="initial">
+                <b>Title: </b>
+                {selectedComplaint.title}
+              </Typography>
+              <Typography mt={2} color="initial">
+                {" "}
+                <b>Description: </b>
+                {selectedComplaint.description}
+              </Typography>
+              {selectedComplaint.image && (
+                <Box mt={2}>
+                  <img
+                    src={`http://localhost:5000/uploads/${selectedComplaint.image}`}
+                    alt="imgg"
+                    style={{
+                      width: "100%",
+                      objectFit: "cover",
+                      height: "300px",
+                      borderRadius: "12px",
+                    }}
+                  />
+                </Box>
+              )}
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                Close
+              </Button>
+            </DialogActions>
+          </>
+        )}
+      </Dialog>
     </Box>
   );
 };
